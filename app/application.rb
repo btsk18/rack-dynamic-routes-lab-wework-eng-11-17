f3e0a56d1125840ad2ba00a78142182bcc5554e4
Brandon Leeds <brandonleeds@gmail.com>
2018-01-31 18:27:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,8 @@ class Application
       item = @@items.find { |s| s.name == item_name }
       resp.write item.price
     else
-        resp.status = 404
+      resp.write "Route not found"
+      resp.status = 404
     end
 
     resp.finish
