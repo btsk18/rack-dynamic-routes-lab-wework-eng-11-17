46191aab11652fd8e9c3cfb705cf8e5fb1721671
Brandon Leeds <brandonleeds@gmail.com>
2018-01-31 18:26:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
     if req.path.match(/item/)
       item_name = req.path.split("/item/").last
       item = @@items.find { |s| s.name == item_name }
+      resp.write item.price
     else
         resp.status = 404
     end
