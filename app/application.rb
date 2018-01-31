6fa916146f96dd922c9b0e5184676ff2dd3da5e6
Brandon Leeds <brandonleeds@gmail.com>
2018-01-31 18:26:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,14 @@
 class Application
+
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/item/)
-
+      item_name = req.path.split("/item/").last
+      item = @@items.find { |s| s.name == item_name }
     else
         resp.status = 404
     end
